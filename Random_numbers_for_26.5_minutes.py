f78ab52250ab7be5c50d8e71a4de92045adf7ee4
againzeenox <VIPTremorYT@gmail.com>
2022-06-24 11:51:15 +0530

    init

--- Random_numbers_for_26.5_minutes.py
+++ Random_numbers_for_26.5_minutes.py
@@ -1,80 +1,30 @@
 from manim import *
-
 import random
 
-class VectorArrow(
-    Scene
-):
-
-    def construct(
-        self
-    ):
+class VectorArrow(Scene):
+    def construct(self):
     
-        num = VGroup(
-
-        )
-
+        num = VGroup()
         i = 0
+        for k in range(10):
 
-        for k in range(
-            10
-        ):
-
-            n = DecimalNumber(
-
-            ).shift(
-                RIGHT * i
-            ).scale(
-                0.5
-            )
-
-            num.add(
-                n
-            )
-
+            n = DecimalNumber().shift(RIGHT * i).scale(0.5)
+            num.add(n)
             i += 0.5
 
-            for m in range(
-                10
-            ):
+            for m in range(10):
 
-                def randomize_numbers(
-                    num
-                ):
+                def randomize_numbers(num):
 
                     for n in num:
-                        value = random.uniform(
-                            0, 1
-                        )
-
-                        n.set_value(
-                            value
-                        )
-
+                        value = random.uniform(0, 1)
+                        n.set_value(value)
                         if value > 0.5:
-
-                            n.set_color(
-                                BLUE
-                            )
-
+                            n.set_color(BLUE)
                         else:
+                            n.set_color(RED)
 
-                            n.set_color(
-                                RED
-                            )
+                        self.play(Write(num))
+                        self.play(FadeOut(n))
 
-                        self.play(
-                            Write(
-                                num
-                            )
-                        )
-
-                        self.play(
-                            FadeOut(
-                                n
-                            )
-                        )
-
-                randomize_numbers(
-                    num
-                )
+                randomize_numbers(num)
